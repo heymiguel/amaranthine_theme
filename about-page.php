e452e5df935da56034433710f5fb7db38570bfab
heymiguel <miguelcesarbautista@gmail.com>
2015-08-24 19:44:13 -0400

    finished about page. prepping for css movement

--- about-page.php
+++ about-page.php
@@ -19,61 +19,80 @@ get_header();  ?>
 					<h3>Skills</h3>	
 					<div class="skills">
 						<div class="iconBox">
-							<i class="devicons devicons-responsive"></i>
-							<span class="skillName">css</span>
+							<i class="devicons devicons-sass"></i>
+							<span class="skillName">sass</span>
 						</div>
 						<div class="iconBox">
-							<i class="devicons devicons-responsive"></i>
-							<span class="skillName">css</span>
+							<i class="devicons devicons-sublime"></i>
+							<span class="skillName">sublime</span>
 						</div>
 						<div class="iconBox">
-							<i class="devicons devicons-responsive"></i>
-							<span class="skillName">css</span>
+							<i class="devicons devicons-gulp"></i>
+							<span class="skillName">gulp</span>
 						</div>
 						<div class="iconBox">
-							<i class="devicons devicons-responsive"></i>
-							<span class="skillName">css</span>
+							<i class="devicons devicons-w3c"></i>
+							<span class="skillName">w3c</span>
 						</div>
 						<div class="iconBox">
-							<i class="devicons devicons-responsive"></i>
-							<span class="skillName">css</span>
+							<i class="devicons devicons-bower"></i>
+							<span class="skillName">bower</span>
 						</div>
 						<div class="iconBox">
-							<i class="devicons devicons-responsive"></i>
-							<span class="skillName">css</span>
+							<i class="devicons devicons-trello"></i>
+							<span class="skillName">trello</span>
 						</div>
 						<div class="iconBox">
-							<i class="devicons devicons-responsive"></i>
-							<span class="skillName">css</span>
+							<i class="devicons devicons-npm"></i>
+							<span class="skillName">npm</span>
 						</div>
 						<div class="iconBox">
-							<i class="devicons devicons-responsive"></i>
-							<span class="skillName">css</span>
+							<i class="devicons devicons-less"></i>
+							<span class="skillName">less</span>
 						</div>
 						<div class="iconBox">
-							<i class="devicons devicons-responsive"></i>
-							<span class="skillName">css</span>
+							<i class="devicons devicons-wordpress"></i>
+							<span class="skillName">wordpress</span>
+						</div>
+						<div class="iconBox">
+							<i class="devicons devicons-gulp"></i>
+							<span class="skillName">gulp</span>
+						</div>
+						<div class="iconBox">
+							<i class="devicons devicons-css3_full"></i>
+							<span class="skillName">css3</span>
+						</div>
+						<div class="iconBox">
+							<i class="devicons devicons-javascript_badge"></i>
+							<span class="skillName">javascript</span>
+						</div>
+						<div class="iconBox">
+							<i class="devicons devicons-jquery"></i>
+							<span class="skillName">JQuery</span>
+						</div>
+						<div class="iconBox">
+							<i class="devicons fa fa-connectdevelop"></i>
+							<span class="skillName">API</span>
 						</div>
 					</div>
 					<h3>What's next</h3>
 		      		<div class="working">
 						<div class="workingBox">
-							<i class="devicons devicons-responsive"></i>
-							<div class="workingDescription"> <p>this is a skill I want to learn. this is also a description of that skill. </p></div>
+							<i class="devicons devicons-nodejs"></i>
+							<div class="workingDescription"> <p>Node.js is a cross-platform runtime environment and a library for running applications written in JavaScript outside the browser (for example, on the server).</p></div>
 						</div>
 						<div class="workingBox">
-							<i class="devicons devicons-responsive"></i>
-							<div class="workingDescription"> <p>this is a skill I want to learn. this is also a description of that skill. </p></div>
+							<i class="devicons devicons-angular"></i>
+							<div class="workingDescription"> <p>AngularJS is an open-source web application framework, maintained by Google and community, that assists with creating single-page applications, one-page web applications that only require HTML, CSS, and JavaScript on the client side.  </p></div>
 						</div>	
 						<div class="workingBox">
-							<i class="devicons devicons-responsive"></i>
-							<div class="workingDescription"> <p>this is a skill I want to learn. this is also a description of that skill. </p></div>
+							<i class="devicons devicons-mongodb"></i>
+							<div class="workingDescription"> <p>MongoDB (from 'humongous') is an open-source document database, and the leading NoSQL database.</p></div>
 						</div>			      			
 		      		</div>			
 		      		<!-- content ends here -->
 		      		<h3>say hey!</h3>
 			      	<div class="container">
-
 			      		<form class="contactForm clearfix"> <?php echo do_shortcode('[contact-form-7 id="76" title="Contact form 1"]'); ?></form>          
 			      	</div>
 		      	</div>
